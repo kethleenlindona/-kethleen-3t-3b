bdd6b24573337da6d5ea8f97cc4b657f1f53c599
kethleenlindona <106692169+kethleenlindona@users.noreply.github.com>
2024-11-14 15:41:58 -0300

    gfcff

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.Jogos Vorazes (2012)} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `A bilheteria do primeiro filme foi de <span>${dados.Jogos_Vorazes_2012}</span>, já do segundo foi de <span>${dados.Jogos_Vorazes_Em_Chamas_2013}</span> em comparação, o terceiro foi dividido em duas partes e obteve um decaimento de <span>${dados.Jogos_Vorazes_A_Esperança_Parte_1_2014}</span> na parte.1 e na parte.2 de <span>${dados.Jogos_Vorazes_A_Esperança_Final_2015}</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
