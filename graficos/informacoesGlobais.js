86dafa138ca5405d48cb116fd5bf0fbbdca19a6d
kethleenlindona <106692169+kethleenlindona@users.noreply.github.com>
2024-11-26 14:30:13 -0300

    26/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `A bilheteria do primeiro filme foi de <span>${dados.Jogos_Vorazes_2012}</span>, já do segundo foi de <span>${dados.Jogos_Vorazes_Em_Chamas_2013}</span> em comparação, o terceiro foi dividido em duas partes e obteve um decaimento de <span>${dados.Jogos_Vorazes_A_Esperança_Parte_1_2014}</span> na parte.1 e na parte.2 de <span>${dados.Jogos_Vorazes_A_Esperança_Final_2015}</span> vizualizações.<br>Isso significa que o filme com aproximadamente <span>${Jogos_Vorazes_A_Cantiga_dos_Pássaros_e_das_Serpentes_2023}%</span> foi o mais asssitido até o presente momento.`
+    paragrafo.innerHTML = `A bilheteria do primeiro filme foi de <span>${dados.Jogos_Vorazes_2012}</span>, já do segundo foi de <span>${dados.Jogos_Vorazes_Em_Chamas_2013}</span> em comparação, o terceiro foi dividido em duas partes e obteve um decaimento de <span>${dados.Jogos_Vorazes_A_Esperança_Parte_1_2014}</span> na parte.1 e na parte.2 de <span>${dados.Jogos_Vorazes_A_Esperança_Final_2015}</span> vizualizações.<br> Em 2023 foi lançado um novo filme com aproximadamente <span>${dados.Jogos_Vorazes_A_Cantiga_dos_Pássaros_e_das_Serpentes_2023}%</span> de vizualizações.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
