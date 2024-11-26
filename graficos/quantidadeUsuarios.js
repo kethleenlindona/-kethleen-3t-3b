063ae6efa75b72d776b4063fd302854387a314de
kethleenlindona <106692169+kethleenlindona@users.noreply.github.com>
2024-11-26 14:15:40 -0300

    26/11

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/pmatjayme/api/refs/heads/main/ket.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
